3a2132bd8306e98f94abe0b52680b9a3af2a4245
Davide Principi <davide.principi@nethesis.it>
2018-04-09 11:49:38 +0200

    Escape HTML in view

--- root/usr/share/nethesis/NethServer/Template/Mattermost/Main.php
+++ root/usr/share/nethesis/NethServer/Template/Mattermost/Main.php
@@ -3,7 +3,7 @@
 echo $view->header()->setAttribute('template', $T('Mattermost_Title'));
 
 $url = $view->getModuleUrl('/Pki');
-$info = "<div class='mattermost-info'><p><span class='fa fa-info-circle'></span>".$T('info_label', array($T('LetsEncryptDomains_label'),$T('Pki_Title')))."</p></div>";
+$info = "<div class='mattermost-info'><p><span class='fa fa-info-circle'></span>" . htmlspecialchars($T('info_label', array($T('LetsEncryptDomains_label'),$T('Pki_Title')))) . "</p></div>";
 
 echo $view->panel()
     ->insert($view->fieldsetSwitch('status', 'enabled', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)->setAttribute('uncheckedValue', 'disabled')
