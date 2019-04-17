84a33ed0e567887f1f62dc286017643e796afa25
Davide Principi <davide.principi@nethesis.it>
2019-04-17 12:34:21 +0200

    ui. Use fetchTranslatedStrings() language API

--- ui/i18n/lang.js
+++ ui/i18n/lang.js
@@ -1,8 +1,8 @@
-var LANG = parent.cockpit.language || 'en'
+
 var LANG_OBJ = {}
 
-$.get("i18n/locale-" + LANG + ".json", function (data) {
-    LANG_OBJ = data
+nethserver.fetchTranslatedStrings(function (data) {
+    LANG_OBJ = data;
     $(document).trigger("nethserver-loaded");
 });
 
