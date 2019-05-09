05bef38d4b1fc9160187a3c9e8cedc099e43f114
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-05-09 09:42:07 +0200

    ui. preserve route on page reload

--- ui/js/app.js
+++ ui/js/app.js
@@ -5,30 +5,33 @@ $(document).on("nethserver-loaded", function () {
 
         this.get('#/', function (context) {
             context.app.swap('');
+            localStorage.setItem("path", '');
             context.render('views/dashboard.html', {})
                 .appendTo(context.$element());
         });
 
         this.get('#/configuration', function (context) {
             context.app.swap('');
+            localStorage.setItem("path", 'configuration');
             context.render('views/configuration.html', {})
                 .appendTo(context.$element());
         });
 
         this.get('#/logs', function (context) {
             context.app.swap('');
+            localStorage.setItem("path", 'logs');
             context.render('views/logs.html', {})
                 .appendTo(context.$element());
         });
 
         this.get('#/about', function (context) {
             context.app.swap('');
+            localStorage.setItem("path", 'about');
             context.render('views/about.html', {})
                 .appendTo(context.$element());
         });
 
         this.before('.*', function () {
-
             var hash = document.location.hash.replace("/", "");
             hash = hash == '#' ? '#dashboard' : hash
             $("nav>ul>li").removeClass("active");
@@ -37,7 +40,8 @@ $(document).on("nethserver-loaded", function () {
 
     });
 
-    app.run('#/');
+    var path = localStorage.getItem("path") || '';
+    app.run('#/' + path);
 
     /* i18n */
     $('[i18n]').each(function () {
